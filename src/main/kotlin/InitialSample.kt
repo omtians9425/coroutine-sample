edb1e347226a5fdd1e861128a10a55a498aea5e9
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-31 20:27:16 +0900

    implement sample of coroutineScope builder

--- src/main/kotlin/InitialSample.kt
+++ src/main/kotlin/InitialSample.kt
@@ -1,7 +1,4 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
     println("#####helloWorld")
@@ -19,31 +16,37 @@ fun main() {
     println("#####helloWorldWithStructuredConcurrency")
     helloWorldWithStructuredConcurrency()
     println("#####\n")
+
+    println("#####helloWorldWithStructuredConcurrency2")
+    helloWorldWithStructuredConcurrency2()
+    println("#####\n")
+
+    coroutineScopeSample()
 }
 
 fun helloWorld() {
     GlobalScope.launch {
-        delay(1000L)
+        delay(100L)
         println("World")
     }
     println("Hello,")
-    Thread.sleep(1100L)
+    Thread.sleep(200L)
 }
 
 fun helloWorldWithBlocking() = runBlocking { //main coroutine
-    GlobalScope.launch { //new coroutine that doesn't block main coroutine
+    GlobalScope.launch { //new coroutine that doesn't block main coroutine/
         println("suspended")
-        delay(1000L)
+        delay(100L)
         println("World")
     }
     println("Hello,")
-    delay(2000L)
+    delay(200L) //still need to wait because the scope of launch() is not parent scope but Global scope.
 }
 
 
 fun helloWorldWithWaiting() = runBlocking{
     val job = GlobalScope.launch {
-        delay(1000L)
+        delay(100L)
         println("World")
     }
     println("Hello,")
@@ -53,13 +56,44 @@ fun helloWorldWithWaiting() = runBlocking{
 /**
  * Structured Concurrency version.
  * A New Coroutine is build using the scope of its parent coroutine so there is no need to join() explicitly.
- * The parent coroutine waits for all coroutines that start in parent scope to complete.
+ * The parent coroutine waits for all coroutines that start in parent(or its child) scope to complete.
  * This principle also enables make error handling more easy.
  */
 fun helloWorldWithStructuredConcurrency() = runBlocking { //parent, main coroutine
-    launch {
-        delay(1000L)
+    launch {//"this" is omitted
+        delay(100L)
         println("World")
     }
     println("Hello,")
+}
+
+fun helloWorldWithStructuredConcurrency2() = runBlocking { //parent, main coroutine
+    /**
+     *  This is also a coroutine builder that creates a new scope.
+     *  This blocks parent coroutine except for new coroutines created in this coroutine.
+     */
+    coroutineScope {
+        delay(100L)
+        println("Hello,")
+    }
+    println("World")
+}
+
+fun coroutineScopeSample() = runBlocking {
+    launch {
+        delay(200L)
+        println("2. task from runBlocking")
+    }
+
+    //This new scope is child scope of runBlocking scope.
+    coroutineScope {
+        launch {
+            delay(500L)
+            println("3. task from nested launch")
+        }
+
+        delay(100L)
+        println("1. task from coroutineScope")
+    }
+    println("4. scope ended.")
 }
