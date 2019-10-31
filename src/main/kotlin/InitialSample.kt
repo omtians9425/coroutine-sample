3ab2c591131b71302cd17c1c469f219ae081e185
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-31 11:52:09 +0900

    implement runblocking

--- src/main/kotlin/InitialSample.kt
+++ src/main/kotlin/InitialSample.kt
@@ -1,12 +1,29 @@
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
+    helloWorld()
+    println("#####")
+    helloWorldWithBlocking()
+}
+
+fun helloWorld() {
     GlobalScope.launch {
         delay(1000L)
         println("World")
     }
-    println("Hello")
+    println("Hello,")
     Thread.sleep(1100L)
+}
+
+fun helloWorldWithBlocking() = runBlocking { //main coroutine
+    GlobalScope.launch { //new coroutine that doesn't block main coroutine
+        println("suspended")
+        delay(1000L)
+        println("World")
+    }
+    println("Hello,")
+    delay(2000L)
 }
